cf5ffbd8cca786e5b396a8b0da733ab66f00293e
RobertODriscoll <G00209099@gmit.ie>
2016-11-13 16:29:14 +0000

    Script finding average of 5 temperatures

--- PythonScripts/tempScript.py
+++ PythonScripts/tempScript.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 import sys
+import datetime
 import Adafruit_DHT
 
-while True:
+today = datetime.date.today()
+tempList = []
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+for x in range(0, 5):
+	tempList = [temperature]
+	
+newTempVar = sum(tempList) / float(len(tempList))
+print newTempVar
+	
 
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+
+
+   
