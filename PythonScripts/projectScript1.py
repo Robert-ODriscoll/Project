e6b3064631e8b0587bb05afd5cb13c23b697abea
RobertODriscoll <G00209099@gmit.ie>
2016-12-10 13:36:27 +0000

    Basic sensor functionality integrated and uploading to database

---
+++ PythonScripts/projectScript1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import sys 			######## import libraries 
+import datetime
+import Adafruit_DHT
+import MySQLdb
+import time
+import RPi.GPIO as GPIO 
+
+GPIO.setmode(GPIO.BCM)
+
+triggerPin = 23         ########## declare pin variables
+echoPin = 24 
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)     ######## call adafruit
+
+GPIO.setup(triggerPin,GPIO.OUT)  ####### set triggerPin as output and echoPin as input
+GPIO.setup(echoPin,GPIO.IN)              
+
+database = MySQLdb.connect(host='localhost',user='root',passwd='root',db='Project_Database')# link to database
+cursor = database.cursor()
+tempList = []  ###### create an array to strore tempreture readings 
+
+epoch = int(time.time()) ###### return epoch value
+
+for x in range(0, 10):  ######### this for loop reads ten tempreture values into tempList and returns the average in newTempVar
+        tempList = [temperature]
+newTempVar = sum(tempList) / float(len(tempList))
+
+GPIO.output(triggerPin, False)     #### sets trigger low
+time.sleep(5) 			   ##### delay for 5 seconds to allow the sensor to calibrate a good reading 
+
+GPIO.output(triggerPin, True)      ##### sets the trigger high
+time.sleep(0.00001)	           #####allow for delay of 0.00001 seconds
+GPIO.output(triggerPin, False)     ######sets the trigger back to low 
+
+while GPIO.input(echoPin)==0:
+	lastLowPulse = time.time()
+while GPIO.input(echoPin)==1:
+	lastHighPulse = time.time()
+
+difference = lastHighPulse - lastLowPulse
+	
+tempdistance = difference * 17150
+distance = round(tempdistance, 2)    
+
+cursor.execute(''' INSERT INTO `sensors`(`epoch`, `temp`,`distance`) VALUES (%s,%s,%s) ''',(epoch,newTempVar,distance)) ###### inserts data into my database 
+
+database.commit() ######### commits to database 
+
+
+
+
--- PythonScripts/tempScript.py
+++ PythonScripts/tempScript.py
@@ -2,17 +2,34 @@
 import sys
 import datetime
 import Adafruit_DHT
+import MySQLdb
+import time
 
-today = datetime.date.today()
+database = MySQLdb.connect(host='localhost',user='root',passwd='root',db='Project_Database')# link to database
+
+
+
+
+cursor = database.cursor()
 tempList = []
 humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
-for x in range(0, 5):
+Epoch = int(time.time())
+print "The Epoch is:  %s." % Epoch
+for x in range(0, 10):
 	tempList = [temperature]
 	
+
 newTempVar = sum(tempList) / float(len(tempList))
-print newTempVar
-	
+
+#cursor.execute(''' INSERT INTO `temperature`(`temp`) VALUES (%s,CURRENT_TIMESTAMP) ''',(newTempVar))
+cursor.execute(''' INSERT INTO `temperature`(`epoch`, `temp`) VALUES (%s,%s) ''',(Epoch,newTempVar))
+
+
+database.commit()
+#time.sleep(1)
+#print newTempVar
+#print dateTime	
 
 
 
